bcfeae1d368c115755cd0d68df5a1e37bf1f8ad5
SinlessMirror <loganpederson@gmail.com>
2023-04-26 11:56:49 -0500

    Card instead of carousel

--- src/Components/PortfolioShowcase.js
+++ src/Components/PortfolioShowcase.js
@@ -1,45 +1,73 @@
 import React from 'react';
-import { Container, Carousel } from 'react-bootstrap';
-import './PortfolioShowcase.css';
-import SimCompanies from '../Img/simcompanies_800x275.png'
-import PopularNews from '../Img/popular_news_800x275.png'
+import { Card, Container, Row, Col, ListGroup } from 'react-bootstrap';
+import calc from '../Img/simcompanies_800x400.png';
+import popularNews from '../Img/popular_news_800x400.png';
+import './PortfolioShowcase.css'
 
+const projects = [
+  {
+    title: 'Popular News',
+    description: [
+      'Full-stack web application',
+      'Real-time news from third-party API',
+      'Robust user authentication',
+      'Personalized user experience with AWS RDS',
+      'Hosted on Ubuntu EC2, served by Nginx',
+    ],
+    imageUrl: popularNews,
+    projectUrl: 'https://popularnews.org',
+  },
+  {
+    title: 'SimCompanies Calculator',
+    description: [
+      'Full-stack web application',
+      'Robust user authentication',
+      'Personalized user experience with AWS RDS',
+      'Real-time news from third-party API',
+      'Hosted on Ubuntu EC2, served by Nginx',
+    ],
+    imageUrl: calc,
+    projectUrl: 'https://simcompaniescalculator.org',
+  },
+  {
+    title: 'Popular News',
+    description: [
+      'Full-stack web application',
+      'Robust user authentication',
+      'Personalized user experience with AWS RDS',
+      'Real-time news from third-party API',
+      'Hosted on Ubuntu EC2, served by Nginx',
+    ],
+    imageUrl: popularNews,
+    projectUrl: 'https://popularnews.org',
+  },
+];
 
-const PortfolioShowcase = () => {
-  const projects = [
-    {
-      imageUrl: SimCompanies,
-    },
-    {
-      imageUrl: PopularNews,
-    },
-    {
-      imageUrl: 'https://via.placeholder.com/800x400',
-    },
-  ];
-
+const ProjectShowcase = () => {
   return (
-    <section className="portfolio-showcase">
-      <Container>
-        <h2 className="text-center mb-5">Project Showcase</h2>
-        <Carousel>
-          {projects.map((project, index) => (
-            <Carousel.Item key={index}>
-              <img
-                className="d-block w-100"
-                src={project.imageUrl}
-                alt={`Slide showing ${project.title}`}
-              />
-              <Carousel.Caption>
-                <h3>{project.title}</h3>
-                <p>{project.description}</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          ))}
-        </Carousel>
-      </Container>
-    </section>
+    <Container className='portfolio-showcase'>
+      <h2 className="portfolio-showcase text-center mb-5">Project Showcase</h2>
+      <Row>
+        {projects.map((project, index) => (
+          <Col key={index} md={4} style={{paddingBottom:'17px'}}>
+            <Card className="mb-4" style={{ minHeight: '100%'}}>
+              <Card.Img variant="top" src={project.imageUrl} />
+              <Card.Body>
+                <Card.Title>{project.title}</Card.Title>
+                <ListGroup variant="flush">
+                  {project.description.map((point, i) => (
+                    <ListGroup.Item key={i}>{point}</ListGroup.Item>
+                  ))}
+                </ListGroup>
+                <Card.Link href={project.projectUrl} target="_blank">
+                  View Project
+                </Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 };
-
-export default PortfolioShowcase;
+export default ProjectShowcase;
